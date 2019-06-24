770c31a5bb328f3361d4222bbc23bfc1cee6591e
yinghui0920 <532681083@qq.com>
2019-06-24 14:33:18 +0800

    逯映辉修订

--- LoansItemMvc/Controllers/LoanItemController.cs
+++ LoansItemMvc/Controllers/LoanItemController.cs
@@ -11,17 +11,43 @@ using System.Web.Mvc;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 using LoansItemMvc.ServiceReference2;
+using LoansItemMvc.ServiceClient;
+using LoansItemMvc.ServiceReference4;
+
 
 namespace LoansItemMvc.Controllers
 {
     public class LoanItemController : Controller
     {
+        AddOneClient client = new AddOneClient();
+        GetLoginClient getlogin = new GetLoginClient();
 
         // GET: LoanItem
         //登录、注册
         public ActionResult Login()
         {
             return View();
+        }
+        /// <summary>
+        /// 注册
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public int Add(ServiceClient.YCUserInfoData data)
+        {
+            var result = client.DoWork1(data);
+            Session["name"] = data.U_Id;
+            return 1;
+        }
+        /// <summary>
+        /// 登陆
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public int DengLu(ServiceReference4.YCUserInfoData data)
+        {
+            var result = getlogin.DoWork(data);
+            return 1;
         }
 
         //首页
@@ -101,74 +127,68 @@ namespace LoansItemMvc.Controllers
         private const String method = "POST";
         private const String appcode = "7ef435ea1fea453ebfa0fcd3430b51bc";
 
-        public void MessageType(string tel)
-        {
-            Random r = new Random();
-            int n = r.Next(100000, 999999);
-            Session["code"] = n;
-        }
+      
 
         //获取验证码
-        //    public string MessageType(string tel)
-        //    {
-        //        Random r = new Random();
-        //        var n = r.Next(100000, 999999);
-        //        Session["value"] = n;
-        //        String querys = "mobile=" + tel + "&param=code:" + n + "&tpl_id=TP1711063";
-        //        String bodys = "";
-        //        String url = host + path;
-        //        HttpWebRequest httpRequest = null;
-        //        HttpWebResponse httpResponse = null;
+            public string MessageType(string tel)
+        {
+            Random r = new Random();
+            var n = r.Next(100000, 999999);
+            Session["value"] = n;
+            String querys = "mobile=" + tel + "&param=code:" + n + "&tpl_id=TP1711063";
+            String bodys = "";
+            String url = host + path;
+            HttpWebRequest httpRequest = null;
+            HttpWebResponse httpResponse = null;
 
-        //        if (0 < querys.Length)
-        //        {
-        //            url = url + "?" + querys;
-        //        }
+            if (0 < querys.Length)
+            {
+                url = url + "?" + querys;
+            }
 
-        //        if (host.Contains("https://"))
-        //        {
-        //            ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(CheckValidationResult);
-        //            httpRequest = (HttpWebRequest)WebRequest.CreateDefault(new Uri(url));
-        //        }
-        //        else
-        //        {
-        //            httpRequest = (HttpWebRequest)WebRequest.Create(url);
-        //        }
-        //        httpRequest.Method = method;
-        //        httpRequest.Headers.Add("Authorization", "APPCODE " + appcode);
-        //        if (0 < bodys.Length)
-        //        {
-        //            byte[] data = Encoding.UTF8.GetBytes(bodys);
-        //            using (Stream stream = httpRequest.GetRequestStream())
-        //            {
-        //                stream.Write(data, 0, data.Length);
-        //            }
-        //        }
-        //        try
-        //        {
-        //            httpResponse = (HttpWebResponse)httpRequest.GetResponse();
-        //        }
-        //        catch (WebException ex)
-        //        {
-        //            httpResponse = (HttpWebResponse)ex.Response;
-        //        }
+            if (host.Contains("https://"))
+            {
+                ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(CheckValidationResult);
+                httpRequest = (HttpWebRequest)WebRequest.CreateDefault(new Uri(url));
+            }
+            else
+            {
+                httpRequest = (HttpWebRequest)WebRequest.Create(url);
+            }
+            httpRequest.Method = method;
+            httpRequest.Headers.Add("Authorization", "APPCODE " + appcode);
+            if (0 < bodys.Length)
+            {
+                byte[] data = Encoding.UTF8.GetBytes(bodys);
+                using (Stream stream = httpRequest.GetRequestStream())
+                {
+                    stream.Write(data, 0, data.Length);
+                }
+            }
+            try
+            {
+                httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+            }
+            catch (WebException ex)
+            {
+                httpResponse = (HttpWebResponse)ex.Response;
+            }
 
-        //        //Console.WriteLine(httpResponse.StatusCode);
-        //        //Console.WriteLine(httpResponse.Method);
-        //        //Console.WriteLine(httpResponse.Headers);
-        //        //Stream st = httpResponse.GetResponseStream();
-        //        //StreamReader reader = new StreamReader(st, Encoding.GetEncoding("utf-8"));
-        //        //Console.WriteLine(reader.ReadToEnd());
-        //        //Console.WriteLine("\n");
-        //        return n.ToString();
+            //Console.WriteLine(httpResponse.StatusCode);
+            //Console.WriteLine(httpResponse.Method);
+            //Console.WriteLine(httpResponse.Headers);
+            //Stream st = httpResponse.GetResponseStream();
+            //StreamReader reader = new StreamReader(st, Encoding.GetEncoding("utf-8"));
+            //Console.WriteLine(reader.ReadToEnd());
+            //Console.WriteLine("\n");
+            return n.ToString();
 
-        //    }
+        }
 
-        //    public static bool CheckValidationResult(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors errors)
-        //    {
-        //        return true;
-        //    }
-        //}
-
+        public static bool CheckValidationResult(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors errors)
+        {
+            return true;
+        }
     }
+
 }
